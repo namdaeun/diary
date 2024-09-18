e55c697ea1e3ec424709e7d60a4b03cc7764267b
namdaeun <nde40345@gmail.com>
2024-09-18 18:35:45 +0900

    init: 스타일 초기 세팅

--- app/root.tsx
+++ app/root.tsx
@@ -6,17 +6,17 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import styles from "~/styles/global.css";
+import "./styles/reset.css";
 
 export const links: LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
   {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
+    rel: "stylesheet",
+    href: "https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard.min.css",
   },
   {
     rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+    href: styles,
   },
 ];
 
---
+++ app/styles/colors.ts
@@ -0,0 +1,14 @@
+export const colors = {
+  white: "#FFFFFF",
+  gray_100: "#F4F4F5",
+  gray_200: "#E8E8EA",
+  gray_300: "#BABABF",
+  gray_400: "#A1A1AA",
+  gray_500: "#696A75",
+  gray_600: "#3B3C4A",
+  gray_700: "#242535",
+  gray_800: "#181A2A",
+  gray_900: "#141624",
+  primary_blue: "#4B6BFB",
+  navy_bg: "#242535",
+};
