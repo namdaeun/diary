f75773553876e25de2c529fb1bbe6f971693ca65
namdaeun <nde40345@gmail.com>
2024-09-28 19:23:43 +0900

    feat: 스위치를 통한 theme 전환

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,19 +1,30 @@
 import type { MetaFunction } from "@remix-run/node";
 import { Link } from "@remix-run/react";
+import Switch from "~/components/switch/Switch";
+import { Theme, useTheme } from "~/context/ThemeProvider";
 
 export const meta: MetaFunction = () => {
-  return [
-    { title: "daLog" },
-    { name: "description", content: "다로그, 다 기록하는 기술블로그" },
-  ];
+  return [{ title: "diary" }, { name: "description", content: "다이어리" }];
 };
 
 export default function Index() {
+  // 후에 헤더로 옮길 theme 코드
+  const [theme, setTheme] = useTheme();
+
+  const toggleTheme = () => {
+    setTheme((prev) => (prev === Theme.light ? Theme.dark : Theme.light));
+  };
+
   return (
     <div className="mx-auto mt-16 max-w-7xl text-center">
       <Link to="/posts" className="text-xl text-blue-600 underline">
         Blog Posts
       </Link>
+
+      <Switch
+        mode={theme === Theme.light ? "light" : "dark"}
+        onChange={toggleTheme}
+      />
     </div>
   );
 }
