8eb1566c9a6f7e19d6daebee786350b40c548f47
namdaeun <nde40345@gmail.com>
2025-08-04 23:33:04 +0900

    chore: 더미 수정

--- app/constants/review.ts
+++ app/constants/review.ts
@@ -1,21 +1,21 @@
 export const REVIEWS = [
   {
     id: 1,
-    name: '이용훈',
+    name: '짱구',
     info: 'CEMWARE 직장 동료',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nec urna ac tellus volutpat viverra. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.',
   },
   {
     id: 2,
-    name: '이용훈',
+    name: '철수',
     info: 'CEMWARE 직장 동료',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nec urna ac tellus volutpat viverra. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.',
   },
   {
     id: 3,
-    name: '이용훈',
+    name: '맹구',
     info: 'CEMWARE 직장 동료',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nec urna ac tellus volutpat viverra. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.',
