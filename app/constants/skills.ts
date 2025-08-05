2338658ccea6daad0e2e8a71ae2dffee21186619
namdaeun <nde40345@gmail.com>
2025-08-05 17:40:03 +0900

    feat: 스킬 섹션 구현

---
+++ app/constants/skills.ts
@@ -0,0 +1,22 @@
+export const SKILLS = [
+  {
+    id: 1,
+    name: 'React',
+    img: '/images/img_react.png',
+  },
+  {
+    id: 2,
+    name: 'TypeScript',
+    img: '/images/img_typescript.png',
+  },
+  {
+    id: 3,
+    name: 'JavaScript',
+    img: '/images/img_javascript.png',
+  },
+  {
+    id: 4,
+    name: 'Next.js',
+    img: '/images/img_nextjs.png',
+  },
+];
